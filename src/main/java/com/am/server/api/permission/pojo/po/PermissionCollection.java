a89128df3e5ea3ede9e3df6e1b8214606a212c98
ruanxuefeng <r19931229>
2020-06-28 15:09:43 +0800

    fix: 添加无参构造函数

--- src/main/java/com/am/server/api/permission/pojo/po/PermissionCollection.java
+++ src/main/java/com/am/server/api/permission/pojo/po/PermissionCollection.java
@@ -1,16 +1,27 @@
 package com.am.server.api.permission.pojo.po;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
+import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
 
 /**
  * @author 阮雪峰
  * @date 2020年6月23日09:44:05
  */
-@AllArgsConstructor
-@Data
+@Getter
+@Setter
 public class PermissionCollection {
     private TreeSet<PermissionTreeDo> set;
+
+    private List<String> permissionList;
+
+    public PermissionCollection() {
+    }
+
+    public PermissionCollection(TreeSet<PermissionTreeDo> set, List<String> permissionList) {
+        this.set = set;
+        this.permissionList = permissionList;
+    }
 }
