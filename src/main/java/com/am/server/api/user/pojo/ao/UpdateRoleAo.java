d1f834bf8de1579ccba9af0aeae77c2d4858e13d
ruanxuefeng <r19931229>
2020-07-04 12:11:45 +0800

    fix: 添加校验

--- src/main/java/com/am/server/api/user/pojo/ao/UpdateRoleAo.java
+++ src/main/java/com/am/server/api/user/pojo/ao/UpdateRoleAo.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -15,6 +16,7 @@ import java.util.List;
 @ApiModel
 @Data
 public class UpdateRoleAo {
+    @NotNull(message = "common.operate.primaryKey.null")
     @ApiModelProperty(value = "用户主键", example = "1234567890987654321", required = true)
     private Long id;
     @ApiModelProperty(value = "角色主键数组", example = "[1234567890987654321,1234567890987654321]")
